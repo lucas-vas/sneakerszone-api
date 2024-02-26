1e879772c1781608bbf45b8d23bc26db5cc90d26
lucas-vas <lucasp90vas@outlook.com>
2024-02-25 22:12:28 -0300

    Configuring cors

--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowSpecificOrigin",
+            builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            });
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
